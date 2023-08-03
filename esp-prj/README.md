1f14ace60dcfe20cd1dd4430e61020c5122bd916
Andy Brychenko <andybrychenko@gmail.com>
2023-08-03 17:14:03 +0300

    esp-prj: Add a template to build a esp project with the esp-idf toolchain

---
+++ esp-prj/README.md
@@ -0,0 +1,5 @@
+### Setup
+
+Run:
+
+`./setup.sh`
---
+++ esp-prj/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -u
+
+IMG_NAME="esp_dev_img"
+CONTAINER_NAME="esp_dev"
+
+USERNAME="esp"
+MOUNT_PATH="$(pwd)"
+
+if [[ ! -d "$(pwd)/esp-idf" ]]; then
+    git clone --recursive https://github.com/espressif/esp-idf.git
+fi
+
+# remove previously created images
+if [[ -n $(docker ps -a | grep "$CONTAINER_NAME") ]]; then
+    docker rm "$CONTAINER_NAME" || true
+    docker rmi "$IMG_NAME" || true
+fi
+
+# create the image
+docker build -t $IMG_NAME --build-arg USERNAME=$USERNAME .
+
+# create the container
+docker create -it --privileged \
+    --mount type=bind,source="$MOUNT_PATH",target=/home/$USERNAME \
+    --name $CONTAINER_NAME $IMG_NAME
